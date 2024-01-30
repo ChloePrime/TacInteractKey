6275834da55e0dbd131cbf9f6a52e57bf8b23363
ChloePrime <chloe_von_einzbern@qq.com>
2024-01-31 02:27:13 +0800

    修复按键效果滞留的bug，版本1.2.2

--- src/main/java/mod/chloeprime/tacinteractkey/client/TacInteractKeyClient.java
+++ src/main/java/mod/chloeprime/tacinteractkey/client/TacInteractKeyClient.java
@@ -1,14 +1,14 @@
 package mod.chloeprime.tacinteractkey.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.tac.guns.client.GunConflictContext;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class TacInteractKeyClient {
     public static final KeyMapping KEY_GUN_INTERACT = new KeyMapping(
-            "key.tacinteractkey.interact", KeyConflictContext.IN_GAME,
+            "key.tacinteractkey.interact", GunConflictContext.IN_GAME_HOLDING_WEAPON,
             InputConstants.Type.KEYSYM, InputConstants.KEY_F, "key.categories.inventory"
     );
 
